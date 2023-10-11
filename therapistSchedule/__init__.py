7b9e1a7ee1b1bf918053de0b62ca089a0da5f85d
shwetaj033 <92169047+shwetaj033@users.noreply.github.com>
2023-10-11 23:05:06 +0100

    updates schedule logic for admin/therapist

--- therapistSchedule/__init__.py
+++ therapistSchedule/__init__.py
@@ -2,19 +2,43 @@ import logging
 
 import azure.functions as func
 import pyodbc
-import os
+import json 
 
 connection_string = "Driver={ODBC Driver 17 for SQL Server};Server=tcp:hackunamatata.database.windows.net,1433;Database=StaySafeDb;Uid=useradmin;Pwd=admin@123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
-
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     curr = get_conn().cursor()
-    curr = curr.execute("SELECT * from Booking;")
+    body = req.get_json()
+
     try:
-        return func.HttpResponse(str(curr.fetchall()))
-    except e:
+        userid = body['userid']
+        usertype= body['usertype']
+        if usertype== 'Admin': 
+            curr = curr.execute(f"SELECT Admin.ID AS AdminID,Admin.Name AS AdminName,Therapist.ID AS TherapistID,Therapist.Name AS TherapistName,Booking.ID AS BookingID,Booking.ClientID,Booking.AppointmentStartTime,Booking.AppointmentEndTime,LoginCredentials.Username AS AdminUsername,LoginCredentials.Password AS AdminPassword FROM Admin INNER JOIN LoginCredentials ON Admin.UserID = LoginCredentials.UserID INNER JOIN Therapist ON Admin.ID = Therapist.AdminID INNER JOIN Booking ON Therapist.ID = Booking.TherapistID Where LoginCredentials.UserID= '{userid}';")
+         
+        elif usertype == 'Therapist': 
+            curr = curr.execute(f"SELECT Therapist.ID AS TherapistID,Therapist.Name AS TherapistName,LoginCredentials.Username AS TherapistUsername FROM Therapist INNER JOIN LoginCredentials ON Therapist.UserID = LoginCredentials.UserID Where LoginCredentials.UserID= '{userid}';")
+        else:  
+           
+            curr = curr.execute(f"SELECT LoginCredentials.UserID, Client.Name AS ClientName,Booking.ID AS BookingID,Booking.AppointmentStartTime,Booking.AppointmentEndTime FROM LoginCredentials INNER JOIN Client ON LoginCredentials.UserID = Client.ID INNER JOIN Booking ON Client.ID = Booking.ClientID WHERE LoginCredentials.UserID = '{userid}';")
+        
+        user = curr.fetchone()
+        
+        if user is not None:
+        
+            return func.HttpResponse(
+                json.dumps({
+                "user_id": user[0],
+                "client_type": user[1],
+                "hash": "verysecurehash"
+                }),
+
+            mimetype="application/json")
+        
+        return func.HttpResponse("Not authorized", status_code=401)
+    except Exception as e:
         return func.HttpResponse(str(e), status_code=500)
     
     
