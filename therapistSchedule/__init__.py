2b29d48759e848c1a7ee0d94fb7e223f16bbcded
shwetaj033 <92169047+shwetaj033@users.noreply.github.com>
2023-10-12 00:04:46 +0100

    updated the schedule logic

--- therapistSchedule/__init__.py
+++ therapistSchedule/__init__.py
@@ -17,23 +17,40 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         usertype= body['usertype']
 
         if usertype== 'Admin': 
-            curr = curr.execute(f"SELECT Admin.ID AS AdminID,Admin.Name AS AdminName,Therapist.ID AS TherapistID,Therapist.Name AS TherapistName,Booking.ID AS BookingID,Booking.ClientID,Booking.AppointmentStartTime,Booking.AppointmentEndTime,LoginCredentials.Username AS AdminUsername,LoginCredentials.Password AS AdminPassword FROM Admin INNER JOIN LoginCredentials ON Admin.UserID = LoginCredentials.UserID INNER JOIN Therapist ON Admin.ID = Therapist.AdminID INNER JOIN Booking ON Therapist.ID = Booking.TherapistID Where LoginCredentials.UserID= '{userid}';")
-         
+            curr = curr.execute(f"SELECT A.TherapistName,C.Name AS ClientName,A.AppointmentStartTime,A.AppointmentEndTime FROM(SELECT Admin.ID AS AdminID,Admin.Name AS AdminName,Therapist.ID AS TherapistID,Therapist.Name AS TherapistName,Booking.ID AS BookingID,Booking.ClientID,Booking.AppointmentStartTime,Booking.AppointmentEndTime,LoginCredentials.Username AS AdminUsername,LoginCredentials.Password AS AdminPasswordFROM Admin INNER JOIN LoginCredentials ON Admin.UserID = LoginCredentials.UserID INNER JOIN Therapist ON Admin.ID = Therapist.AdminID INNER JOIN Booking ON Therapist.ID = Booking.TherapistID WHERE LoginCredentials.UserID = '{userid}') AS INNER JOIN Client AS C ON A.ClientID = C.ID;")
+            schedules = curr.fetchall()
+            processed_schedules = []
+
+            for schedule in schedules: 
+                processed_schedules.append({
+               "therapistname": user[0],
+                "clientname": user[1],
+                "appointmentst": user[2],
+                "appointmentet": user[3]
+                })
         elif usertype == 'Therapist': 
-            curr = curr.execute(f"SELECT Therapist.ID AS TherapistID,Therapist.Name AS TherapistName,LoginCredentials.Username AS TherapistUsername FROM Therapist INNER JOIN LoginCredentials ON Therapist.UserID = LoginCredentials.UserID Where LoginCredentials.UserID= '{userid}';")
+            curr = curr.execute(f"T.TherapistName,T.TherapistUsername,SELECT T.TherapistID,,C.Name AS ClientName FROM(SELECT Therapist.ID AS TherapistID,Therapist.Name AS TherapistName,LoginCredentials.Username AS TherapistUsername FROM Therapist INNER JOIN LoginCredentials ON Therapist.UserID = LoginCredentials.UserID WHERE LoginCredentials.UserID = '{userid}') AS T INNER JOIN Booking ON T.TherapistID = Booking.TherapistID INNER JOIN Client AS C ON Booking.ClientID = C.ID;")
+            schedules = curr.fetchall()
+            processed_schedules = []
+
+            for schedule in schedules: 
+                processed_schedules.append({
+               "therapistname": user[0],
+                "therapistid": user[1],
+                "clientname": user[2]})
         else:  
            
-            curr = curr.execute(f"SELECT LoginCredentials.UserID, Client.Name AS ClientName,Booking.ID AS BookingID,Booking.AppointmentStartTime,Booking.AppointmentEndTime FROM LoginCredentials INNER JOIN Client ON LoginCredentials.UserID = Client.ID INNER JOIN Booking ON Client.ID = Booking.ClientID WHERE LoginCredentials.UserID = '{userid}';")
-        
-        schedules = curr.fetchall()
-        
+            curr = curr.execute(f"SELECT T.Name AS TherapistName,T.PhoneNumber AS TherapistPhoneNumber,B.AppointmentStartTime,B.AppointmentEndTime FROM LoginCredentials AS LC INNER JOIN Client AS C ON LC.UserID = C.ID INNER JOIN Booking AS B ON C.ID = B.ClientID INNER JOIN Therapist AS T ON B.TherapistID = T.ID WHERE LC.UserID ='{userid}';")
+            schedules = curr.fetchall()
+            processed_schedules = []
 
-        processed_schedules = []
-
-        for schedule in schedules: 
-            processed_schedules.append({
-                ""
-            })
+            for schedule in schedules: 
+                processed_schedules.append({
+               "therapistname": user[0],
+                "therapistph": user[1],
+                "appointmentst": user[2],
+                "appointmentet": user[3]})
+        
 
         return func.HttpResponse(
             json.dumps({"schedule": processed_schedules}),
