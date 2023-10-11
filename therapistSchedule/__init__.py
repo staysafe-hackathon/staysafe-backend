3d5b34f6c01735e7a3ad352ed231878ddee9a0f6
shwetaj033 <92169047+shwetaj033@users.noreply.github.com>
2023-10-11 19:18:11 +0100

    added therapist schedule

--- therapistSchedule/__init__.py
+++ therapistSchedule/__init__.py
@@ -1,24 +1,25 @@
 import logging
 
 import azure.functions as func
+import pyodbc
+import os
+
+connection_string = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:hackunamatata.database.windows.net,1433;Database=StaySafeDb;Uid=useradmin;Pwd=admin@123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    curr = get_conn().cursor()
+    curr = curr.execute("SELECT * from Booking;")
+    try:
+        return func.HttpResponse(str(curr.fetchall()))
+    except e:
+        return func.HttpResponse(str(e), status_code=500)
+    
+    
+def get_conn():
+    conn = pyodbc.connect(connection_string)
+    conn.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
+    conn.setencoding(encoding='utf-8')
+    return conn
