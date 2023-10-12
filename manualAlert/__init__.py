862cb877fcb787779c242548dbd39affe42a51e1
shwetaj033 <92169047+shwetaj033@users.noreply.github.com>
2023-10-12 11:14:28 +0100

    updated id name

--- manualAlert/__init__.py
+++ manualAlert/__init__.py
@@ -2,39 +2,44 @@ import logging
 
 import azure.functions as func
 import pyodbc
-import json 
+import os
+import datetime
 
 connection_string = "Driver={ODBC Driver 17 for SQL Server};Server=tcp:hackunamatata.database.windows.net,1433;Database=StaySafeDb;Uid=useradmin;Pwd=admin@123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
 
+#app = func.FunctionApp()
+
+#@app.route(route="checkin")
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    curr = get_conn().cursor()
     body = req.get_json()
 
-    try:
-        username = body['bookingid']
-        pw = body['reason']
-        curr = curr.execute(f"SELECT UserID, UserType from LoginCredentials where Username = '{username}' and Password='{pw}';")
-        
-        user = curr.fetchone()
-        
-        if user is not None:
-        
-            return func.HttpResponse(
-                json.dumps({
-                "user_id": user[0],
-                "client_type": user[1],
-                "hash": "verysecurehash"
-                }),
+    booking_id = body["bookingId"]
+    
+    
 
-            mimetype="application/json")
-        
-        return func.HttpResponse("Not authorized", status_code=401)
+    print("booking id is: ", booking_id)
+    print("user id is: ", user_id)
+    print("date is: ", date)
+
+    sql = "INSERT INTO Alert (ID, BookingID , Longitude, Latitude, Address, ReasonForAlert, AlertFlag,ReasonForFlag,IsAuto, IsResolved) VALUES (?, 'YourGeoLocationValue', 'YourAddressValue', 'YourReasonForAlertValue', 1, 'YourReasonForFlagValue', 1, 0)"
+
+    curr = get_conn().cursor()
+    curr = curr.execute(sql, (booking_id, date, None, None, None, lat, long))
+
+    curr.commit()
+
+    try:
+        return func.HttpResponse(str(curr.fetchall()))
     except Exception as e:
         return func.HttpResponse(str(e), status_code=500)
     
     
+def concatenate_geocoords(lat: float, long: float) -> str:
+    concatenated = str(lat) + " " + str(long)
+    return concatenated
+
 def get_conn():
     conn = pyodbc.connect(connection_string)
     conn.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
